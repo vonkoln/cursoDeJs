d56512d99c975ae9c51f110d7912f223bcc321d6
brunovonkoln <brunovonkoln@gmail.com>
2022-11-08 16:14:49 -0300

    race

--- aula108promises/aula108.js
+++ aula108promises/aula108.js
@@ -1,9 +1,51 @@
-function esperaAi(msg, tempo) {
-    setTimeout(() => {
-        console.log(msg)
-    }, tempo)
+function rand(min, max) {
+    min *= 1000
+    max *= 1000
+    return Math.floor(Math.random() * (max - min) + min)
 }
 
-esperaAi('Frase 1', 1000)
-esperaAi('Frase 1', 1000)
-esperaAi('Frase 1', 1000)
+function esperaAi(msg, tempo) {
+    return new Promise((resolve, reject) => {
+        if(typeof msg !== 'string') reject(false)
+
+        setTimeout(() => {
+            resolve(msg)
+        }, tempo)
+    })
+}
+
+esperaAi('Conexão com o BD', rand(1, 3))
+  .then(resposta => {
+    console.log(resposta)
+    return esperaAi('Buscando dados da BASE', rand(1, 3))
+  })
+  .then(resposta => {
+    console.log(resposta)
+    return esperaAi(22222, rand(1, 3))
+  })
+  .then(resposta => {
+    console.log(resposta)
+  })
+  .then(() => {
+    console.log('Exibe dados na tela')
+  })
+
+  .catch(e => {
+    console.log('Erro:', e)
+  })
+
+  console.log('Isso aqui será exibido antes de qualquer promise')
+
+
+
+    
+
+
+
+
+
+// esperaAi('Frase 1', rand(1, 3), function() {
+//     esperaAi('Frase 2', rand(1, 3), function() {
+//       esperaAi('Frase 3', rand(1, 3))
+//     })
+// })
---
+++ aula108promises/aula109.js
@@ -0,0 +1,50 @@
+function rand(min, max) {
+    min *= 1000
+    max *= 1000
+    return Math.floor(Math.random() * (max - min) + min)
+}
+
+function esperaAi(msg, tempo) {
+    return new Promise((resolve, reject) => {
+
+        setTimeout(() => {
+            if(typeof msg !== 'string') {
+             reject('Cai no Erro')
+             return
+            }
+            resolve(msg.toUpperCase() + ' - Passei na promise')
+            return
+        }, tempo)
+    })
+}
+
+function baixaPagina() {
+    const emCache = true
+
+    if(emCache) {
+        return Promise.resolve('Página em cache')
+    } else {
+        return esperaAi('Baixei a página', 3000)
+    }
+}
+
+baixaPagina()
+    .then(dadosPagina => {
+        console.log(dadosPagina)
+    })
+    .catch(e => console.log('ERRO', e))
+//Promise.all Promise.race Promise.resolve Promise.reject
+// const promises = [
+//     esperaAi('Promise 1', rand(1, 5)),
+//     esperaAi('Promise 2', rand(1, 5)),
+//     esperaAi('Promise 3', rand(1, 5)),
+//     esperaAi(1000, rand(1, 5)),
+// ]
+
+// Promise.race(promises)
+//   .then(function(valor) {
+//     console.log(valor)
+//   })
+//   .catch(function(erro) {
+// //     console.log(erro)
+//   })
